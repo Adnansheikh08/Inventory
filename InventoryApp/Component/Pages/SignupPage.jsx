29b8be62ac09b6db644d67f651e72bd060faa8b6
Mohd Adnan Sheikh <adnansheikh3721@gmail.com>
2025-06-17 03:11:38 +0530

    Backend Work Starts

--- InventoryApp/Component/Pages/SignupPage.jsx
+++ InventoryApp/Component/Pages/SignupPage.jsx
@@ -5,7 +5,7 @@ const SignupPage = () => {
     <div className="container mx-60">
       <section className="bg-[#FAFAFA] bg-no-repeat bg-contain bg-[url(https://img.freepik.com/premium-vector/modern-sky-blue-gradient-banner-with-abstract-shapes_278222-3179.jpg)]">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div className="bg-[#FAFAFA] shadow-2xl shadow p-4 w-full rounded-lg sm:max-w-md xl:p-0 dark:border-gray-700">
+          <div className="bg-[#FAFAFA] shadow-2xl  p-4 w-full rounded-lg sm:max-w-md xl:p-0 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl"> Create a new account </h1>
               <form className="space-y-4 md:space-y-6" action="#">
