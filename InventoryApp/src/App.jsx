2c2370311dfa15893705fbcaa529dfd6f682e3fe
Mohd Rizwan <rizwanansari718263@gmail.com>
2025-06-19 20:02:22 +0530

    logout button

--- InventoryApp/src/App.jsx
+++ InventoryApp/src/App.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Toaster } from 'sonner'
-import AddProduct from '../Component/Pages/AddProduct'
+import AddProductLayout from "../Component/Pages/AddProductLayout"
 import DashboardLayout from "../Component/Pages/DashboardLayout"
 import LoginPage from "../Component/Pages/LoginPage"
 import ProductLayout from "../Component/Pages/ProductLayout"
 import SalesPage from '../Component/Pages/SalesPage'
 import SignupPage from "../Component/Pages/SignupPage"
-import AddProductLayout from "../Component/Pages/AddProductLayout"
 
 const App = () => {
   return (
@@ -14,11 +13,9 @@ const App = () => {
       {/* <Sidebar/> */}<Toaster position="top-center" richColors/>
       <Routes>
         <Route path="/" element={<DashboardLayout />} />
-        {/* <Route path="/" element={<ProductPage/>}/> */}
         <Route path="/add-product" element={<AddProductLayout/>}/>
         <Route path="/sales" element={<SalesPage/>}/>
         <Route path="/latest-products" element={<ProductLayout/>}/>
-        {/* <Route path="/latest-products" element={<LatestProducts/>}/> */}
         <Route path="/loginpage" element={<LoginPage/>}/>
         <Route path="/signuppage" element={<SignupPage/>}/>
       </Routes>
