729f5c29455024b4083a9ec0b26898bac4690873
Mohd Rizwan <rizwanansari718263@gmail.com>
2025-06-23 15:33:57 +0530

    Search functionality added and Layout fixed

--- InventoryApp/Component/Pages/LatestProducts.jsx
+++ InventoryApp/Component/Pages/LatestProducts.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from 'react';
 import IceCreamCard from './newCard';
-function LatestProduct() {
+
+const LatestProduct = ( {isSidebarOpen} ) => {
   const [p, setP] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchLProducts, setSearchLProducts] = useState("");
 
  useEffect(() => {
   fetch("http://localhost:5050/product/latestproducts")
@@ -18,20 +19,57 @@ function LatestProduct() {
     .finally(() => setLoading(false));
 }, []);
 
+  const filteredLProducts = p.filter((item) =>
+    item.name.toLowerCase().includes(searchLProducts.toLowerCase())
+  );
+
 
  if (loading) return <p>Loading…</p>;
 if (!p) return <p>No product found.</p>;
 
 return (
  <>
-     <div>
+     <div className={`transition-all duration-300 p-4 ${isSidebarOpen ? "ml-64" : "ml-16"}`}>
        
-       {/* Search Form */}
+      {/* Search Form */}
+
+      <form
+          className="flex items-center max-w-xl min-w-lg"
+          onSubmit={(e) => e.preventDefault()}>   
+          <label for="voice-search" className="sr-only">Search</label>
+          <div className="relative w-full">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                  <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 21">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.15 5.6h.01m3.337 1.913h.01m-6.979 0h.01M5.541 11h.01M15 15h2.706a1.957 1.957 0 0 0 1.883-1.325A9 9 0 1 0 2.043 11.89 9.1 9.1 0 0 0 7.2 19.1a8.62 8.62 0 0 0 3.769.9A2.013 2.013 0 0 0 13 18v-.857A2.034 2.034 0 0 1 15 15Z"/>
+                  </svg>
+              </div>
+
+              <input
+              type="text"
+              id="voice-search"
+              className="text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 border dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search Products"
+              value={searchLProducts}
+              onChange={(e) => setSearchLProducts(e.target.value)}
+              required />
+              <button type="button" className="absolute inset-y-0 end-0 flex items-center pe-3">
+                  <svg className="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7v3a5.006 5.006 0 0 1-5 5H6a5.006 5.006 0 0 1-5-5V7m7 9v3m-3 0h6M7 1h2a3 3 0 0 1 3 3v5a3 3 0 0 1-3 3H7a3 3 0 0 1-3-3V4a3 3 0 0 1 3-3Z"/>
+                  </svg>
+              </button>
+          </div>
+          <button type="submit" className="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+              </svg>Search
+          </button>
+      </form>
        
  
        {/* Cards flex */}
        <div className="flex flex-row flex-wrap justify-streach mt-6 space-x-4 space-y-4">
-         {p.length > 0 ? (p.map(item => (  // For search functionality
+        {filteredLProducts.length > 0 ? (
+          filteredLProducts.map((item) => ( // For search functionality
          <IceCreamCard
            key={item.id}
            id={item.id}
